c2d1cde2d26ad3f075ea30106edc33a56d5001bc
Freezy96 <stenlleycold@yahoo.com>
2018-07-20 17:51:19 +0800

    Setting Up CSS/JS

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,9 +1,10 @@
-<footer>Some footer content</footer>
+<footer>Footer</footer>
    </div><!-- /.main-content -->
   </div><!-- /.dashboard-wrapper -->
 
   <!-- Core Scripts - Include with every page -->
 
-  
+  	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/custom.css">
+	<script type = 'text/javascript' src = "<?php echo base_url(); ?>js/custom.js"></script>
  </body>
 </html>
