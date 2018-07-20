cfc8f7284eda90e64fc2f2e71aadfe74e6e5487f
Freezy96 <stenlleycold@yahoo.com>
2018-07-20 17:33:37 +0800

    Template - Header / Nav / Footer

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('template/header');
+		$this->load->view('template/nav');
 		$this->load->view('welcome_message');
+		$this->load->view('template/footer');
 	}
 }
 
---
+++ application/views/template/footer.php
@@ -0,0 +1,9 @@
+<footer>Some footer content</footer>
+   </div><!-- /.main-content -->
+  </div><!-- /.dashboard-wrapper -->
+
+  <!-- Core Scripts - Include with every page -->
+
+  
+ </body>
+</html>
