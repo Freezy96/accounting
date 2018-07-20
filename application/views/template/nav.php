9acec15f00f92a91be80628c4c7f29788ac52f2b
Freezy96 <stenlleycold@yahoo.com>
2018-07-20 18:34:46 +0800

    Navbar

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -1,2 +1,9 @@
-<div class="navbar navbar-default" role="navigation">
-</div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="...">
+      </a>
+    </div>
+  </div>
+</nav>
