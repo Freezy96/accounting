e250b54b66cb32b213151f7cdb31263ed372f8b6
Freezy96 <stenlleycold@yahoo.com>
2018-08-05 19:25:28 +0800

    Nthing

--- application/models/Account_model.php
+++ application/models/Account_model.php
@@ -6,7 +6,7 @@ class Account_model extends CI_Model{
     
     public function getuserdata(){
         // Run the query
-        $this->db->select('a.accountid, a.oriamount, a.customerid, c.customername, a.amount, a.payment, a.datee, a.interest, a.duedate, a.packageid, ag.agentname, p.name');
+        $this->db->select('a.accountid, a.customerid, c.customername, a.oriamount, a.amount, a.payment, a.datee, a.interest, a.duedate, a.packageid, ag.agentname, p.name');
         $this->db->from('account a');
         $this->db->join('customer c', 'a.customerid = c.customerid', 'left');
         $this->db->join('agent ag', 'a.agentid = ag.agentid', 'left');
