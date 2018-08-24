68c41e94e5a0bc1399170543b6f11e9a3b5cf8a1
qcs07032 <38035501+qcs07032@users.noreply.github.com>
2018-08-24 15:46:34 +0800

    package

--- application/views/Package/insert_15%_week.php
+++ application/views/Package/insert_15%_week.php
@@ -5,8 +5,8 @@
     <input type="number" step="0.01" class="form-control" id="" placeholder="Lent Amount" name="lentamount" required>
   </div>
     <div class="form-group">
-    <label for="">Guaranty Item</label>
-    <input type="text" class="form-control" id="" placeholder="Guaranty Item" name="guarantyitem" required>
+    <label for="">Interest Per Day Late (%)</label>
+    <input type="number" step="0.01" class="form-control" id="" placeholder="Lent Amount" name="interest" required>
   </div>
 
 
