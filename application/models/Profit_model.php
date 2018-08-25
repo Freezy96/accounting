8947ed27d286180d097480897126b5f1b5d36c25
Freezy96 <stenlleycold@yahoo.com>
2018-08-25 21:16:43 +0800

    li

---
+++ application/models/Profit_model.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Security_Model extends CI_Model{
+    function __construct(){
+        parent::__construct();
+    }
+    
+    public function secure_session_login(){
+        // Run the query
+        $userid = $this->session->userdata('adminid');
+        if($userid == "")
+        {
+            echo "<script>alert('Plese Login First !');</script>";
+            echo "<script>window.location.replace('". base_url() ."login');</script>";
+        }
+
+    }
+
+}
+?>
