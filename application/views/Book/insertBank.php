e94379023768f1f9b3dcb7190dd73afedefef382
qcs07032 <38035501+qcs07032@users.noreply.github.com>
2018-09-27 14:00:38 +0800

    book

---
+++ application/views/Book/insertBank.php
@@ -0,0 +1,31 @@
+<?php $this->load->view('template/sidenav'); ?>
+
+<form action='<?php echo base_url();?>Book/insertbank' method='post' name='insert' enctype="multipart/form-data">
+ <div class="form-group">
+    <label for="">Bank Name:</label>
+    <input type="text" class="form-control" id="" placeholder="Bank" name="bank" required>
+  </div>
+<div class="form-group">
+    <label for="">Description:</label>
+    <input type="text" class="form-control" id="" placeholder="Des" name="description" required>
+  </div>
+   <div class="form-group">
+    <label for="">Type:</label>
+    <select name="type">
+      <optgroup>
+        <option value="payment">Payment</option>   
+        <option value="receive">Receive</option>
+      </optgroup>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="">Amount:</label>
+<input type="number" step="0.01" class="form-control" id="" placeholder="Amount" name="amount" required>
+  </div>
+  <div class="form-group">
+    <label for="">Date:</label>
+    <input type="date" class="form-control" id="" name="datee" required>
+  </div>
+</div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
