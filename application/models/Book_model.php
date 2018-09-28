b54c797c3c776f590ab8ec61ced606fb18b6dd84
qcs07032 <38035501+qcs07032@users.noreply.github.com>
2018-09-28 13:07:15 +0800

    model

---
+++ application/models/Book_model.php
@@ -0,0 +1,51 @@
+   <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Customer_Model extends CI_Model{
+    function __construct(){
+        parent::__construct();
+    }
+    public function insertB($data){
+        if($this->db->insert('bank', $data)){
+        	$return = "insert";
+            $id = $this->db->insert_id();
+        	return $id;
+        }else{
+        	$return = "false";
+        	return $return;
+        }
+
+    }
+        public function insertC($data){
+        if($this->db->insert('COH', $data)){
+            $return = "insert";
+            $id = $this->db->insert_id();
+            return $id;
+        }else{
+            $return = "false";
+            return $return;
+        }
+
+    }
+            public function insertE($data){
+        if($this->db->insert('Emp', $data)){
+            $return = "insert";
+            $id = $this->db->insert_id();
+            return $id;
+        }else{
+            $return = "false";
+            return $return;
+        }
+
+    }
+            public function insertT($data){
+        if($this->db->insert('Total', $data)){
+            $return = "insert";
+            $id = $this->db->insert_id();
+            return $id;
+        }else{
+            $return = "false";
+            return $return;
+        }
+
+    }
+    }
+?>
