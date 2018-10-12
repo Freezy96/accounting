eed4d2456ab3e6ee356bf6888816fc067c03e0d1
Freezy96 <stenlleycold@yahoo.com>
2018-10-13 01:57:42 +0800

    optimize

--- application/views/Book/main.php
+++ application/views/Book/main.php
@@ -2,41 +2,42 @@
 
 <div class="row">
 <div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/bank'">Bank</button>
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/bank'">Bank</button>
 </div>
 
 <div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/coh'">Cash On Hand</button>
-</div>
-
-</div>
-<br>
-<div class="row">
-<div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/emp'">Employee</button>
-</div>
-
-<div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/Total'">Total</button>
-</div>
-</div>
-<div class="row">
-<div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/insertbank'">Insert Bank</button>
-</div>
-
-<div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/insertcoh'">Insert Cash On Hand</button>
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/coh'">Cash On Hand</button>
 </div>
 
 </div>
 <br>
 <div class="row">
 <div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/insertemp'">Insert Employee</button>
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/emp'">Employee</button>
 </div>
 
 <div class="col-sm-6">
-	<button style="height: 25vh;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/insertTotal'">Insert Total</button>
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-primary" onclick="location.href='<?php echo base_url();?>book/Total'">Total</button>
+</div>
+</div>
+<br>
+<div class="row">
+<div class="col-sm-6">
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-success" onclick="location.href='<?php echo base_url();?>book/insertbank'">Insert Bank</button>
+</div>
+
+<div class="col-sm-6">
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-success" onclick="location.href='<?php echo base_url();?>book/insertcoh'">Insert Cash On Hand</button>
+</div>
+
+</div>
+<br>
+<div class="row">
+<div class="col-sm-6">
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-success" onclick="location.href='<?php echo base_url();?>book/insertemp'">Insert Employee</button>
+</div>
+
+<div class="col-sm-6">
+	<button style="height: 25vh; font-size: 150%;" class="btn btn-block btn-success" onclick="location.href='<?php echo base_url();?>book/insertTotal'">Insert Total</button>
 </div>
 </div>
